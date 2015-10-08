aea3d4a4e289a504f1a5eaab51e6a7e5c0be429e
mloefler <loe@loe.at>
2015-10-08 23:53:49 +0200

    update put production users into different file (since then tests will fail)

--- pse-dbmodell/SQL/db-create.sql
+++ pse-dbmodell/SQL/db-create.sql
@@ -297,12 +297,6 @@ LOCK TABLES `person` WRITE;
 /*!40000 ALTER TABLE `person` DISABLE KEYS */;
 INSERT INTO `person` VALUES (1,'pse_system','Bulletin Board System User','JBOSS','mario.loefler@edu.fh-joanneum.at','+436644711815','--','system-root',NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,1,NULL);
 INSERT INTO `person` VALUES (2,'padmin','Portal','Admin','pse@edu.fh-joanneum.at','+436644711815','--','Portal-Admin',NULL,NULL,NULL,NULL,NULL,NULL,1,1,0,1,NULL);
-
-INSERT INTO `person` VALUES (90, 'krenn','Krenn','Christian','christian.krenn@imaginary.com','+436644711815','--','Vortragender',NULL,NULL,NULL,NULL,NULL,NULL,1,1,0,0,NULL);
-INSERT INTO `person` VALUES (91, 'teiniker','Teiniker','Egon','egon.teiniker@imaginary.com','+436644711815','--','Vortragender',NULL,NULL,NULL,NULL,NULL,NULL,1,1,0,0,NULL);
-
-INSERT INTO `person` VALUES (100, 'loeflerm13','Löfler','Mario','mario.loefler@edu.fh-joanneum.at','+436644711815','--','Knecht','Team 1','A','-2','666',NULL,NULL,1,1,0,0,NULL);
-/*!40000 ALTER TABLE `person` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
