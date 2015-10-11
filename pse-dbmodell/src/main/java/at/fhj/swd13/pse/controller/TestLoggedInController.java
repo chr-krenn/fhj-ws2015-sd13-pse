ae6133be8ed68adbfda1c72d43de76f35d317989
StefanHaring <haringstefan@gmx.net>
2015-10-11 19:48:30 +0200

    TestLoggedIn: User-data visualisation added
    TestLoggedIn.xhtml: User-data fields added
    TestLoggedInController: New method getUser

--- pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/TestLoggedInController.java
+++ pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/TestLoggedInController.java
@@ -3,6 +3,11 @@ package at.fhj.swd13.pse.controller;
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 
+import at.fhj.swd13.pse.db.CurrentDbContext;
+import at.fhj.swd13.pse.db.DbContext;
+import at.fhj.swd13.pse.db.entity.Person;
+import at.fhj.swd13.pse.domain.user.UserService;
+import at.fhj.swd13.pse.domain.user.UserServiceImpl;
 import at.fhj.swd13.pse.plumbing.UserSession;
 
 @ManagedBean
@@ -11,9 +16,20 @@ public class TestLoggedInController {
 	@Inject
 	private UserSession userSession;
 	
+	@Inject @CurrentDbContext
+	private DbContext dbContext;
+	
+	private UserService userService;
+	
 	
 	public String getUsername() {
 		
 		return userSession.getUsername();
 	} 	
+	
+	public Person getUser()
+	{
+		userService = new UserServiceImpl(dbContext);
+		return userService.getUser(userSession.getUsername());
+	}
 }
