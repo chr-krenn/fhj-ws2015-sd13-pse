5f90fe7d00d035f9153dbc4bc7fab6d07fe28b2a
mloefler <loe@loe.at>
2015-10-11 21:40:23 +0200

    authentication for pages

---
+++ pse-dbmodell/src/main/java/at/fhj/swd13/pse/webfilter/AuthenticationFilter.java
@@ -0,0 +1,58 @@
+package at.fhj.swd13.pse.webfilter;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.logging.Logger;
+
+import at.fhj.swd13.pse.plumbing.UserSession;
+
+@WebFilter("/*")
+public class AuthenticationFilter implements Filter {
+
+	@Inject
+	private Logger logger;
+
+	@Inject
+	private UserSession userSession;
+
+	@Override
+	public void destroy() {}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+
+		logger.info("[AUTH] requesting url " + req.getRequestURI());
+
+		if (req.getRequestURI().indexOf("/protected") < 0 || isLoggedIn(request)) {
+
+			logger.info("[AUTH] ++ allowed");
+			chain.doFilter(request, response);
+
+		} else {
+
+			logger.info("[AUTH] -- denied, redirected to Gandalf");
+
+			HttpServletResponse servletResponse = (HttpServletResponse) response;
+			servletResponse.sendRedirect(req.getContextPath() + "/NotLoggedIn.jsf");
+		}
+	}
+
+	private boolean isLoggedIn(ServletRequest request) {
+		return userSession.isLoggedIn();
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {}
+}
