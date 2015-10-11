b101a2ca6c91636f54f620f6695b34b0370a528a
mloefler <loe@loe.at>
2015-10-11 11:30:33 +0200

    first draft of login capability

---
+++ pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/TestLoggedInController.java
@@ -0,0 +1,19 @@
+package at.fhj.swd13.pse.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import at.fhj.swd13.pse.plumbing.UserSession;
+
+@ManagedBean
+public class TestLoggedInController {
+
+	@Inject
+	private UserSession userSession;
+	
+	
+	public String getUsername() {
+		
+		return userSession.getUsername();
+	} 	
+}
