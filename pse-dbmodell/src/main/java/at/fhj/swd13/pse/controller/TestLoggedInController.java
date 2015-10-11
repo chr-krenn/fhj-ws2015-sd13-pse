55bd748e8e3bc3a3e53b24579bd5224c3287b962
mloefler <loe@loe.at>
2015-10-11 21:13:06 +0200

    throw exception when not logged in

--- pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/TestLoggedInController.java
+++ pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/TestLoggedInController.java
@@ -1,7 +1,9 @@
 package at.fhj.swd13.pse.controller;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
+import javax.ws.rs.NotAuthorizedException;
 
 import at.fhj.swd13.pse.db.entity.Person;
 import at.fhj.swd13.pse.domain.user.UserService;
@@ -16,6 +18,13 @@ public class TestLoggedInController {
 	@Inject
 	private UserService userService;
 
+	@PostConstruct
+	protected void validateLogin() {
+		if ( ! userSession.isLoggedIn() ) {
+			throw new NotAuthorizedException("User has not logged in and is not authorized to view this page");
+		}
+	}
+	
 	public String getUsername() {
 
 		return userSession.getUsername();
