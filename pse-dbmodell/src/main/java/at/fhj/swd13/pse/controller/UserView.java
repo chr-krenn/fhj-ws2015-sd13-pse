3129639b30c8b8c5f68e6ad37ad63b5631fe1820
StefanHaring <haringstefan@gmx.net>
2015-10-16 10:21:59 +0200

    UserNameTag: fixed errors

--- pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/UserView.java
+++ pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/UserView.java
@@ -34,38 +34,8 @@ public class UserView implements Serializable {
 	@Inject
 	private Logger logger;
 
-	@Inject
-	private DocumentService documentService;
-
 	public UserDTO getUserDTO() {
 		String userName = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("userName");
 		return new UserDTO(userService.getUser(userName));
-	}
-	
-	public void handleFileUpload(FileUploadEvent event) {
-
-		logger.info("[UPLOAD] storing: " + event.getFile().getFileName());
-
-		try {
-
-			Document document = documentService.store(event.getFile().getFileName(), event.getFile().getInputstream());
-
-			FacesMessage message = new FacesMessage("In Ordnung", event.getFile().getFileName() + " is uploaded.");
-			FacesContext.getCurrentInstance().addMessage(null, message);
-
-			logger.info("[UPLOAD] file has been uploaded: " + event.getFile().getFileName());
-			
-			userService.setDocument(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("userName"), document);
-
-			
-			
-		} catch (IOException | InvalidPathException | EntityNotFoundException x) {
-
-			logger.error("[UPLOAD] error uploading: " + x.getMessage());
-
-			FacesMessage message = new FacesMessage("Fehler", "Fehler beim hochladen von " + event.getFile().getFileName());
-			message.setSeverity(FacesMessage.SEVERITY_ERROR);
-			FacesContext.getCurrentInstance().addMessage(null, message);
-		}
 	}
 }
