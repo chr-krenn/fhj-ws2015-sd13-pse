5fcdf25b8cb5c10709b94c353ac6c79daff9ecba
Franz Angelo <fangelo>
2015-10-17 12:28:49 +0200

    Added user search

---
+++ pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/UserSearchController.java
@@ -0,0 +1,43 @@
+package at.fhj.swd13.pse.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+import at.fhj.swd13.pse.db.entity.Person;
+import at.fhj.swd13.pse.domain.user.UserService;
+import at.fhj.swd13.pse.dto.UserDTO;
+
+@ManagedBean
+@SessionScoped
+public class UserSearchController {
+
+	private String search = "";
+
+	@Inject
+	private UserService userService;
+
+
+	public List<UserDTO> getResult() {
+		List<UserDTO> result = new ArrayList<UserDTO>();
+		for (Person person : userService.findUsers(getSearch())) {
+			result.add(new UserDTO(person));
+		}
+		return result;
+	}
+
+	public String doSearch() {
+		return "/protected/UserSearchResult.jsf?faces-redirect=true";
+	}
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	}
+}
