9d549cfae87a8d58bcb4360a53350d280ae2a2ee
pompenig13 <christine.pompenig@edu.fh-joanneum.at>
2015-10-18 20:38:04 +0200

    PSE2015-19 - Started Activity Stream on Home page So far the query only excludes expired messages - work in progress!

--- pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/SimpleFeedController.java
+++ pse-dbmodell/src/main/java/at/fhj/swd13/pse/controller/SimpleFeedController.java
@@ -7,9 +7,12 @@ import javax.enterprise.inject.Model;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.OrderBy;
 
 import at.fhj.swd13.pse.db.entity.Message;
 import at.fhj.swd13.pse.domain.feed.FeedService;
+import at.fhj.swd13.pse.domain.user.UserService;
+import at.fhj.swd13.pse.plumbing.UserSession;
 
 /**
  * 
@@ -21,14 +24,30 @@ public class SimpleFeedController {
 
     @Produces
     @Named
+    @OrderBy("createdOn ASC")
 	private List<Message> messages;
     
     @Inject
     private FeedService feedService;
     
+    @Inject
+	private UserService userService;
+    
+    @Inject
+	private UserSession userSession;
+    
     @PostConstruct
     public void postConstruct() {
     	
     	messages = feedService.loadFeed();
+    }
+    
+    public List<Message> getMessages () {
+    	return messages;
+    }
+    
+    public List<Message> getActivities() {
+    	return feedService.loadFeedForUser(userService.getUser(userSession.getUsername()));
+    	
     }
 }
