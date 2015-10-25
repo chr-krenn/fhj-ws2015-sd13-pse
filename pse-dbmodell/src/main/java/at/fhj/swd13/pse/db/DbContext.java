aec7b4d95d3a3fbe32b38ca8cbcba871342e1b24
mloefler <loe@loe.at>
2015-10-25 10:33:35 +0100

    removed warning

--- pse-dbmodell/src/main/java/at/fhj/swd13/pse/db/DbContext.java
+++ pse-dbmodell/src/main/java/at/fhj/swd13/pse/db/DbContext.java
@@ -10,7 +10,6 @@ import at.fhj.swd13.pse.db.dao.MessageDAO;
 import at.fhj.swd13.pse.db.dao.MessageTagDAO;
 import at.fhj.swd13.pse.db.dao.PersonDAO;
 import at.fhj.swd13.pse.db.dao.TagDAO;
-import at.fhj.swd13.pse.db.entity.DocumentLibraryEntry;
 
 /**
  * Connection to a persistent storage service
@@ -22,32 +21,36 @@ public interface DbContext extends AutoCloseable {
 	/**
 	 * Add an object for storage to the persistent storage
 	 * 
-	 * @param target the object to add
+	 * @param target
+	 *            the object to add
 	 */
 	void persist(Object target) throws ConstraintViolationException;
 
 	/**
 	 * Remove an object from the persistent storage
 	 * 
-	 * @param target the object to remove
+	 * @param target
+	 *            the object to remove
 	 */
-	void remove( Object target );
-	
-	
+	void remove(Object target);
+
 	/**
 	 * Write all pending changes since instantiation to the storage
 	 * 
-	 * @throws ConstraintViolationException when an unique index is violated
-	 * @throws IllegalStateException when commit is called 
-	 * 			on an already closed DbContext
+	 * @throws ConstraintViolationException
+	 *             when an unique index is violated
+	 * @throws IllegalStateException
+	 *             when commit is called
+	 *             on an already closed DbContext
 	 */
 	void commit() throws ConstraintViolationException;
 
 	/**
 	 * Undo all pending changes since instantiation to the storage
 	 *
-	 * @throws IllegalStateException when commit is called
-	 * 			 on an already closed DbContext
+	 * @throws IllegalStateException
+	 *             when commit is called
+	 *             on an already closed DbContext
 	 */
 	void rollback();
 
@@ -55,31 +58,33 @@ public interface DbContext extends AutoCloseable {
 	 * remove all cached entities
 	 */
 	void clearCache();
-	
+
 	/**
 	 * Create a query on this transaction with the given query string
-	 *  
-	 * @param queryString the query to create
+	 * 
+	 * @param queryString
+	 *            the query to create
 	 * 
 	 * @return query object
- 	*/
+	 */
 	Query createQuery(String queryString);
-	
+
 	/**
 	 * Create a query on this transaction with the given name
-	 *  
-	 * @param queryName name of the query to instantiate
+	 * 
+	 * @param queryName
+	 *            name of the query to instantiate
 	 * 
 	 * @return query object
- 	*/
-	Query createNamedQuery( String queryName );
+	 */
+	Query createNamedQuery(String queryName);
 
 	/**
 	 * get the underlying entity manager - should not be used
+	 * 
 	 * @return
 	 */
 	EntityManager getEntityManager();
-
 
 	/**
 	 * Get a dao for the person entity and closely related entities
@@ -87,47 +92,46 @@ public interface DbContext extends AutoCloseable {
 	 * @return an instance of a DAO
 	 */
 	PersonDAO getPersonDAO();
-	
+
 	/**
 	 * Get a dao for the tag entity
+	 * 
 	 * @return
 	 */
 	TagDAO getTagDAO();
-	
+
 	/**
 	 * Get a dao for communities
 	 * 
 	 * @return an instance of a DAO
 	 */
 	CommunityDAO getCommunityDAO();
-	
+
 	/**
 	 * Get a dao for documents
 	 * 
 	 * @return an instance of a DAO
 	 */
 	DocumentDAO getDocumentDAO();
-	
+
 	/**
 	 * Get a dao for messages
 	 * 
 	 * @return an instance of a DAO
 	 */
 	MessageDAO getMessageDAO();
-	
+
 	/**
 	 * Get a dao for MessageTags
 	 * 
 	 * @return an instance of a DAO
 	 */
 	MessageTagDAO getMessageTagDAO();
-	
-	
+
 	DocumentLibraryEntryDAO getDocumentLibraryDAO();
-	
-	
+
 	/**
-	 * Close DbContext 
+	 * Close DbContext
 	 * if transaction has not been commited or rollback'ed, a rollback is performed
 	 */
 	@Override
