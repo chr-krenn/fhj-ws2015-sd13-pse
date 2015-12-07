c6222090e32020d287353988da874905e4da59dd
Franz Angelo <fangelo>
2015-12-07 16:56:21 +0100

    moved member top local variable

--- pse-dbmodell/src/test/java/at/fhj/swd13/pse/test/gui/UserSearchIT.java
+++ pse-dbmodell/src/test/java/at/fhj/swd13/pse/test/gui/UserSearchIT.java
@@ -10,15 +10,13 @@ import at.fhj.swd13.pse.test.gui.pageobjects.UserPage;
 
 public class UserSearchIT extends SeleniumBaseTestCase {
 	
-	private static HomePage homepage;
-
 	@Test
 	/*
 	 * PSE2015-9: Als angemeldeter Benutzer des Systems möchte ich nach anderen Benutzern des Systems suchen können
 	 */
 	public void testSearchUser() {
 		login("florian.genser", "12345678");
-		homepage = new HomePage(driver);
+		HomePage homepage = new HomePage(driver);
 
 		UserList userList = homepage.searchUser("ei");
 		verifyEquals(2, userList.getUsers().size());
