59a4b42423246a331e67cc37e91872a052d2c391
oswaldge13 <oswaldge13>
2016-01-02 11:05:27 +0100

    Completed the selenium test for the password reset

--- pse/src/test/java/at/fhj/swd13/pse/test/gui/UserForgotPasswordIT.java
+++ pse/src/test/java/at/fhj/swd13/pse/test/gui/UserForgotPasswordIT.java
@@ -6,12 +6,14 @@ import org.junit.Test;
 
 import at.fhj.swd13.pse.test.base.SeleniumBaseTestCase;
 import at.fhj.swd13.pse.test.gui.pageobjects.LoginPage;
+import at.fhj.swd13.pse.test.gui.pageobjects.NotLoggedInPage;
 import at.fhj.swd13.pse.test.gui.pageobjects.ResetPasswordPage;
 
 public class UserForgotPasswordIT extends SeleniumBaseTestCase{
 
 	private ResetPasswordPage resetPasswordPage = null;
 	private LoginPage loginPage;
+	private NotLoggedInPage notLoggedInPage;
 	private static final String RESET_PASSWORD_HEADER = "Kennwort zurücksetzen";
 	private static final String RESET_STATUS_EMAIL_INVALID = "An die E-Mail-Adresse konnte nicht gesendet werden.";
 	
@@ -30,9 +32,29 @@ public class UserForgotPasswordIT extends SeleniumBaseTestCase{
 	
 	
 	@Test
-	public void testResetEmptyPassword() {		
+	public void testResetEmptyEmail() {		
 		resetPasswordPage.resetPassword("");
 		verifyEquals(RESET_STATUS_EMAIL_INVALID, resetPasswordPage.getStatusText());
+	}
+	
+	
+	@Test
+	public void testLoginWithOldPassword() {
+		final String username = "oswaldge13";
+		final String passwordOld = "12345678";
+		final String emailAddress = "gerald.oswald@edu.fh-joanneum.at";
+		
+		resetPasswordPage.resetPassword(emailAddress);
+		
+		//need to wait for some seconds to have the password reset and the e-mail sent
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		notLoggedInPage = loginPage.loginWithWrongCredentials(username, passwordOld);
+		
+		verifyTrue(notLoggedInPage.isDoorsOfDurinLabelPresent());			
 	}
 	
 	@After
