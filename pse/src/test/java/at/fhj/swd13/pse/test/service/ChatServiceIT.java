d37b8943b23d61f327f9ba55fc4943e2278c2890
StefanHaring <haringstefan@gmx.net>
2016-01-08 21:24:02 +0100

    Added tests to ChatServiceIT

--- pse/src/test/java/at/fhj/swd13/pse/test/service/ChatServiceIT.java
+++ pse/src/test/java/at/fhj/swd13/pse/test/service/ChatServiceIT.java
@@ -1,9 +1,13 @@
 package at.fhj.swd13.pse.test.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.NamingException;
 
@@ -69,5 +73,71 @@ public class ChatServiceIT extends RemoteTestBase {
     	
     	assertNull(chatService.getCommunity(unconfirmedCommunity.getCommunityId()));
     }
+    
+    /*
+     * PSE2015-58 Als angemeldeter Benutzer habe ich auf der Übersichtsseite der Communities eine Liste aller öffentlichen und privaten Communities  
+     */
+    @Test
+    public void getAllAssessibelCommunities()
+    {
+    	List<Community> communities = new ArrayList<Community>();
+    	
+    	communities = chatService.getAllAccessibleCommunities();
+    	assertNotNull(communities);
+    	assertEquals(communities.size(), 3);
+    	
+    	Community communityPublicCommunity = chatService.getCommunity("Public community");
+    	assertNotNull(communityPublicCommunity);
+    	assertTrue(communities.contains(communityPublicCommunity));
+    	
+    	Community communitySWD = chatService.getCommunity("SWD");
+    	assertNotNull(communitySWD);
+    	assertTrue(communities.contains(communitySWD));
+    	
+    	Community communityPrivate = chatService.getCommunity("Private community");
+    	assertNotNull(communityPrivate);
+    	assertTrue(communities.contains(communityPrivate));
+    }
+    
+    @Test
+    public void GetUnconfirmedCommunities() {
+    	assertEquals(1, chatService.getUnconfirmedCommunities().size());
+    	Community unconfirmedCommunity = chatService.getUnconfirmedCommunities().get(0);
+    	assertNull(unconfirmedCommunity.getConfirmedBy());
+    	assertTrue(unconfirmedCommunity.getName().equals("Not confirmed community"));
+    }
+    
+    @Test
+    public void GetCommunity() {
+    	Community community = chatService.getCommunity("Not confirmed community");
+    	assertNotNull(community);
+    	assertNull(community.getConfirmedBy());
+    	assertTrue(community.getName().equals("Not confirmed community"));
+    }
+    
+    @Test
+    public void PersonNotMemberOfCommunity()
+    {
+    	Community community = chatService.getCommunity("Not confirmed community");
+    	assertNotNull(community);
+    	
+    	Person admin = userService.getUser("padmin");
+    	assertNotNull(admin);
+    	
+    	assertFalse(chatService.isPersonMemberOfCommunity(admin, community));
+    }
+    
+    @Test
+    public void PersonMemberOfCommunity()
+    {
+    	Community community = chatService.getCommunity("SWD");
+    	assertNotNull(community);
+    	
+    	Person user = userService.getUser("pompenig13");
+    	assertNotNull(user);
+    	
+    	assertTrue(chatService.isPersonMemberOfCommunity(user, community));
+    }
+ 
 }
  
