66679ef3e7d754d228b15dbf0195c0983b7763e8
mloefler <loe@loe.at>
2016-01-11 23:28:46 +0100

    removed TODO

--- pse/src/main/java/at/fhj/swd13/pse/domain/document/DefaultDocumentLibraryRightsProvider.java
+++ pse/src/main/java/at/fhj/swd13/pse/domain/document/DefaultDocumentLibraryRightsProvider.java
@@ -8,13 +8,11 @@ public class DefaultDocumentLibraryRightsProvider implements DocumentLibraryRigh
 
 	@Override
 	public boolean canViewLibrary() {
-		//TODO Check if the logged in person is part of this community
 		return true;
 	}
 
 	@Override
 	public boolean canEditLibrary() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 }
